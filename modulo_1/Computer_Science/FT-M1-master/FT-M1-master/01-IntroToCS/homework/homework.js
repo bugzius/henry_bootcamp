34b229bbb521709ae544b69eb303ab4944a089ba
daprionil <davidarenaoflol23@gmail.com>
2023-01-24 15:56:44 -0500

    HomeWork 01: Computer Science

---
+++ modulo_1/Computer_Science/FT-M1-master/FT-M1-master/01-IntroToCS/homework/homework.js
@@ -0,0 +1,31 @@
+'use strict';
+
+function BinarioADecimal(num) {
+   //Toma un número en string binario y entrega un número decimal
+   const arrNum = [...num].reverse();
+   let numFinal = 0;
+
+   for(let i = 0; i < arrNum.length; i++){
+      if(arrNum[i] === '1'){
+         const numComp = Math.pow(2,i);
+         numFinal += numComp;
+      }
+   }
+
+   return numFinal;
+}
+
+function DecimalABinario(num) {
+   let numberFinal = '';
+   
+   for(let i = num; i > 0; i = ( ( i - ( i % 2 ) ) / 2 ) ){
+      numberFinal += `${ i % 2 }`
+   };
+
+   return [...numberFinal].reverse().join().replaceAll(',','');
+}
+
+module.exports = {
+   BinarioADecimal,
+   DecimalABinario,
+};
