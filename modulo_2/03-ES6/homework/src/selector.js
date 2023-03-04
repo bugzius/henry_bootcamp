5c597afe8c80307f0909e10c8c9aca9bd7d57d24
daprionil <davidarenaoflol23@gmail.com>
2023-03-04 16:28:04 -0500

    M2 03-ES6 Homework: Completed

--- modulo_2/03-ES6/homework/src/selector.js
+++ modulo_2/03-ES6/homework/src/selector.js
@@ -1,37 +1,57 @@
-var traverseDomAndCollectElements = function(matchFunc, startEl) {
+var traverseDomAndCollectElements = function(matchFunc, startEl = document.body) {
   var resultSet = [];
-
-  if (typeof startEl === "undefined") {
-    startEl = document.body;
-  }
 
   // recorre el árbol del DOM y recolecta elementos que matchien en resultSet
   // usa matchFunc para identificar elementos que matchien
 
   // TU CÓDIGO AQUÍ
-  
+  const {childElementCount,children} = startEl;
+  if( childElementCount === 0){
+		console.log({validate: matchFunc(startEl), startEl})
+    if(matchFunc(startEl)){
+      resultSet.push(startEl);
+    }
+  }
+  if(childElementCount > 0){
+		if(matchFunc(startEl)) resultSet.push(startEl);
+    for(let child of children){
+      resultSet = [...resultSet, ...traverseDomAndCollectElements(matchFunc, child)];
+    };
+  };
+
+  return resultSet;
 };
 
 // Detecta y devuelve el tipo de selector
 // devuelve uno de estos tipos: id, class, tag.class, tag
 
 
-const selectorTypeMatcher = function(selector) {
+function selectorTypeMatcher(selector) {
   // tu código aquí
-  let arrSplit = [];
-  let iCurrent = 0;
-  [...selector].forEach((element,i) => {
-      if(['#','.'].includes(element)){
-        if(arrSplit.length === 0){
-            firstSelector = selector.slice(0, iCurrent);
-            arrSplit.push(firstSelector);
-        }
-        valSelector = selector.slice(iCurrent,i);
-        arrSplit.push(valSelector);
-        iCurrent = i;
-      };
-  });
-  return arrSplit;
+  const values = {'#':'id', '.':'class'};
+  return [...selector].reduce( (init, l, i) => {
+    const {elements, iCurrent} = init;
+
+    if(i === selector.length - 1){
+      elements.push(selector.slice(iCurrent, i + 1));
+    }
+
+    if(['#','.'].includes(l)){
+      elements.push(selector.slice(iCurrent, i ));
+      init.iCurrent = i;
+    }
+
+    return init;
+  }, {elements:[], iCurrent:0}).elements.reduce((init,l) => {
+    const reg = l.match(/[.|#]/);
+    if(reg){
+      const [val] = reg;
+      init.push(`${init.length ? val : ''}${values[val]}`);
+    }else if(l){
+      init.push('tag');
+    }
+    return init;
+  },[]).join('');
 };
 
 // NOTA SOBRE LA FUNCIÓN MATCH
@@ -39,20 +59,21 @@ const selectorTypeMatcher = function(selector) {
 // parametro y devuelve true/false dependiendo si el elemento
 // matchea el selector.
 
-var matchFunctionMaker = function(selector) {
-  var selectorType = selectorTypeMatcher(selector);
-  var matchFunction;
-  if (selectorType === "id") { 
-   
-  } else if (selectorType === "class") {
-    
-  } else if (selectorType === "tag.class") {
-    
-  } else if (selectorType === "tag") {
-    
+function matchFunctionMaker(selector) {
+  const selectorType = selectorTypeMatcher(selector);
+  const [cero, first] = selector.split(/[.|#]/);
+
+  const matchFunction = {
+    id: ({id}) => id === first,
+    class: ({classList}) => classList.contains(first),
+    ['tag.class']: ({localName, classList}) => classList.contains(first) && localName === cero,
+    tag: ({localName}) => localName === cero
   }
-  return matchFunction;
+
+  return matchFunction[selectorType];
 };
+
+
 
 var $ = function(selector) {
   var elements;
