028554a5f6b81faa37c1ba0776c8d2b4c4020e38
daprionil <davidarenaoflol23@gmail.com>
2023-04-18 19:14:20 -0500

    M3 03-Promises: Homework Complete

--- FT-M3-master/03-Promises/homework/01 - Exercises/exercise-two.js
+++ FT-M3-master/03-Promises/homework/01 - Exercises/exercise-two.js
@@ -33,19 +33,25 @@ function problemA() {
    * arreglo es rechazada, la promesa del método irá directamente a el
    * reject de la promesa.
    */
-  const promises = [
+  //PROMISES
+  exerciseUtils.promisifiedReadFile("poem-two/stanza-01.txt")
+    .then(stanza => exerciseUtils.blue(stanza));
+  
+  exerciseUtils.promisifiedReadFile("poem-two/stanza-02.txt")
+    .then(stanza => exerciseUtils.blue(stanza));
+
+  
+  //PROMISE ALL
+  /* const promises = [
     exerciseUtils.promisifiedReadFile("poem-two/stanza-01.txt"),
     exerciseUtils.promisifiedReadFile("poem-two/stanza-02.txt")
   ];
-  const resultsPromises = Promise.all([...promises]);
-  resultsPromises.then(([stanza1, stanza2]) => {
-    console.log([stanza1, stanza2], 'severo ÑERO');
-    exerciseUtils.blue(stanza1);
-    exerciseUtils.blue(stanza2);
-  })
-  .catch(reason => exerciseUtils.magenta(reason));
-
-}
+  Promise.all([...promises])
+    .then(([stanza1,stanza2]) => {
+      exerciseUtils.blue(stanza1);
+      exerciseUtils.blue(stanza2);
+    }); */
+};
 
 function problemB() {
   let filenames = [1, 2, 3, 4, 5, 6, 7, 8].map(function (n) {
@@ -64,6 +70,28 @@ function problemB() {
 
   // promise version
   // Tu código acá:
+  
+  //Prop Of Concept
+  
+  /* const promises = filenames.map( filename => exerciseUtils.promisifiedReadFile(filename));
+  const valuesPromises = Promise.allSettled(promises);
+  
+  valuesPromises.then( data => {
+    for(let i = 0; i < data.length; i++){
+      const {status, value} = data[i];
+      if(status === 'rejected'){
+        exerciseUtils.magenta(value);
+        continue;
+      };
+      exerciseUtils.blue(value);
+    }
+  }); */
+  
+  filenames.forEach((filename) => {
+    exerciseUtils.promisifiedReadFile(filename)
+      .then( stanza => exerciseUtils.blue(stanza)
+        ,reason => exerciseUtils.magenta(new Error(reason)));
+  });
 }
 
 // EJERCICIO EXTRA
